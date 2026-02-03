ce7a769c426d710ebeea37acdb9e05d395e9b1d5
Subham Mohanty <159928393+subhamohanty07@users.noreply.github.com>
2026-02-03 11:14:22 +0530

    Add files via upload

---
+++ P_1822.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int i, count = 0;
+        for(i=0; i<nums.size(); i++){
+            if(nums[i]==0)
+                return 0;
+            else if(nums[i]<0)
+                count++;
+        }
+        if(count%2==0)
+            return 1;
+        else
+            return -1;
+    }
+};
