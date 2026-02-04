101cab2c3566e18c182f8be741aa17744ffeb5cf
Subham Mohanty <159928393+subhamohanty07@users.noreply.github.com>
2026-02-04 19:32:43 +0530

    Add files via upload

---
+++ P_203.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<ListNode.h>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head && head->val == val) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+    ListNode* curr = head;
+
+    while (curr && curr->next) {
+        if (curr->next->val == val) {
+            ListNode* temp = curr->next;
+            curr->next = temp->next;
+            delete temp;
+        } else {
+            curr = curr->next;
+        }
+    }
+    return head;
+    }
+};
