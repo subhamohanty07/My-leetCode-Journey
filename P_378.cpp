268d9f71ba6083687ab69674dce11cb7a1071d3b
Subham Mohanty <159928393+subhamohanty07@users.noreply.github.com>
2026-03-09 20:35:56 +0530

    Add files via upload

---
+++ P_378.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+        vector<int> arr;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                arr.push_back(matrix[i][j]);
+            }
+        }
+
+        sort(arr.begin(), arr.end());
+
+        return arr[k-1];
+    }
+};
